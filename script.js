d389bcb3ea13a0363b294507344ba880554f69a6
muddasir0 <smhbalti12@gmail.com>
2020-11-14 19:16:02 +0500

    index  change

--- script.js
+++ script.js
@@ -19,7 +19,25 @@ function countdown(){
 function formatTime(time){
     return time<10?('0${time}'):time;
 }
+import { useState } from 'react'
 
+function SimpleToggle() {
+  const [isOn, setIsOn] = useState(false)
+  return (
+    <span
+      role="checkbox"
+      aria-checked={isOn}
+      tabIndex="0"
+      onClick={() => setIsOn(!isOn)}
+      class={`${isOn ? 'bg-indigo-600' : 'bg-gray-200'} relative inline-block flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-outline`}
+    >
+      <span
+        aria-hidden="true"
+        className={`${isOn ? 'translate-x-5' : 'translate-x-0'} inline-block h-5 w-5 rounded-full bg-white shadow transform transition ease-in-out duration-200`}
+      ></span>
+    </span>
+  )
+}
 //initial call
 countdown();
 setInterval(countdown,1000);
